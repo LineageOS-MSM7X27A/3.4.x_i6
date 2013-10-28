1462d8a58f6848b9a38f00c708f9393c42b53840
Praveen Chavan <pchavan@codeaurora.org>
2013-10-28 00:44:14 -0700

    msm: vidc: cache-flush encoder output buffers after alloc
    Flush output buffers explcitly during registering. This ensures
    any pending CPU writes (i.e meset to 0 after allocation) are
    committed right away, or else this may get flushed _after_
    hardware has written the bitstream.
    
    Change-Id: I958466011e7d96c7b5e2691403c8386309997429
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>
    Signed-off-by: c_sridur <sridur@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/vidc/common/enc/venc_internal.c

--- drivers/video/msm/vidc/common/enc/venc_internal.c
+++ drivers/video/msm/vidc/common/enc/venc_internal.c
@@ -1632,6 +1632,7 @@ u32 vid_enc_set_buffer(struct video_client_ctx *client_ctx,
 		}
 	}
 
+
 	vcd_status = vcd_set_buffer(client_ctx->vcd_handle,
 				    vcd_buffer_t, (u8 *) kernel_vaddr,
 				    buffer_info->sz);
@@ -1721,7 +1722,7 @@ u32 vid_enc_encode_frame(struct video_client_ctx *client_ctx,
 				&buff_handle);
 
 		if (vcd_input_buffer.data_len > 0) {
-			if (ion_flag == CACHED && buff_handle) {
+			if (ion_flag == ION_FLAG_CACHED && buff_handle) {
 				msm_ion_do_cache_op(
 				client_ctx->user_ion_client,
 				buff_handle,
@@ -1880,7 +1881,7 @@ u32 vid_enc_set_recon_buffers(struct video_client_ctx *client_ctx,
 					0,
 					(unsigned long *)&iova,
 					(unsigned long *)&buffer_size,
-					UNCACHED, 0);
+					0, 0);
 			if (rc || !iova) {
 				ERR(
 				"%s():ION map iommu addr fail, rc = %d, iova = 0x%lx\n",
